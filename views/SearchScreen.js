499dcc1ba2058abdb52f9ee891130dfbd2b6b1d8
amine <bouamama.amine@bcg.com>
2018-12-10 17:11:55 +0100

    make query to search

--- views/SearchScreen.js
+++ views/SearchScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, TextInput, Image, ScrollView } from "react-native";
+import { StyleSheet, View, TextInput, Image, ScrollView, Alert } from "react-native";
 import axios from "axios"
 import Octicons from "@expo/vector-icons/Octicons";
 import { Card, CardItem, Text, Body, Right, Icon, List, ListItem, Thumbnail, Left, Button, Spinner } from "native-base";
@@ -16,22 +16,38 @@ export default class SearchScreen extends React.Component {
     };
   }
 
-  async componentDidMount(){
 
-      let query = "enima";
-
-      let [response_users, response_repos] = await Promise.all([
-        axios.get(`https://api.github.com/search/users?q=${query}`),
-        axios.get(`https://api.github.com/search/repositories?q=${query}`),
-      ]);
-
-      let [resultUsers, resultRepos] = [
-        response_users.data.items.slice(0, 8),
-        response_repos.data.items.slice(0, 8)
-      ];
-
-      this.setState({resultUsers, resultRepos})
+  async querySearchUsers(query){
+    try{
+      let response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+        let resultUsers = response.data.items.slice(0, Math.min(8, response.data.items.length));
+      this.setState({resultUsers, userIsLoading: false})
+    } catch (err) {
+      Alert.alert('Search Limit Reached', 'You have reached the maximum number of searches per minute allowed. Please wait a moment, or subscribe to our premium plan 😉');
+      this.setState({userIsLoading: false})
+    }
   }
+
+  async querySearchRepos(query){
+    try{
+      let response = await axios.get(`https://api.github.com/search/repositories?q=${query}`);
+      let resultRepos = response.data.items.slice(0, Math.min(8, response.data.items.length));
+      this.setState({resultRepos, repoIsLoading: false});
+    } catch (err) {
+      Alert.alert('Search Limit Reached', 'You have reached the maximum number of searches per minute allowed. Please wait a moment, or subscribe to our premium plan 😉');
+      this.setState({repoIsLoading: false})
+    }
+  }
+
+  handleSearchBarInput = query => {
+    this.setState({query});
+  };
+
+  handleOnSubmitSearch = async () => {
+    this.setState({userIsLoading: true, repoIsLoading: true});
+    this.querySearchUsers(this.state.query);
+    this.querySearchRepos(this.state.query);
+  };
 
   render() {
 
@@ -84,43 +100,45 @@ export default class SearchScreen extends React.Component {
         <View style={styles.searchBar}>
           <TextInput
             style={styles.input}
-            onChangeText={query => this.setState({ query })}
+            onChangeText={this.handleSearchBarInput}
             value={this.state.query}
             autoFocus={true}
           />
-          <Octicons size={20} name="search"  />
+          <Button transparent onPress={this.handleOnSubmitSearch}><Octicons size={20} name="search"  /></Button>
+
         </View>
 
         {/*SEARCH RESULTS*/}
-
         <Card style={styles.resultContainer}>
           <ScrollView>
 
-          <CardItem header bordered>
-            <Text>Users</Text>
-          </CardItem>
+            {/*USERS RESULTS*/}
+            <CardItem header bordered>
+              <Text>Users</Text>
+            </CardItem>
 
-          <CardItem bordered>
-            <List transparent style={styles.innerList}>
-              {this.state.userIsLoading ?
-                <Spinner/> :
-                userResultHTML
-              }
-            </List>
-          </CardItem>
-          {/*--------------------------------------*/}
-          <CardItem header bordered>
-            <Text>Repositories</Text>
-          </CardItem>
+            <CardItem bordered>
+              <List transparent style={styles.innerList}>
+                {this.state.userIsLoading ?
+                  <Spinner/> :
+                  userResultHTML
+                }
+              </List>
+            </CardItem>
 
-          <CardItem bordered>
-            <List transparent style={styles.innerList}>
-              {this.state.repoIsLoading ?
-                <Spinner/> :
-                repoResultHTML
-              }
-            </List>
-          </CardItem>
+            {/*REPO RESULTS*/}
+            <CardItem header bordered>
+              <Text>Repositories</Text>
+            </CardItem>
+
+            <CardItem bordered>
+              <List transparent style={styles.innerList}>
+                {this.state.repoIsLoading ?
+                  <Spinner/> :
+                  repoResultHTML
+                }
+              </List>
+            </CardItem>
 
           </ScrollView>
         </Card>
