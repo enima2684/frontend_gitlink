057db676c80eac77f9c78319bb1e2837afe95abd
amine <bouamama.amine@bcg.com>
2018-12-11 20:08:48 +0100

    header done

--- views/OneRepositoryScreen.js
+++ views/OneRepositoryScreen.js
@@ -7,7 +7,7 @@ import {
   ActivityIndicator,
   Alert
 } from "react-native";
-import { Container, H1, List, ListItem, H2, Thumbnail, Button, Spinner } from "native-base";
+import { Container, H1, List, ListItem, H2, Thumbnail, Button, Spinner, H3 } from "native-base";
 import Octicons from "@expo/vector-icons/Octicons";
 import requestBuilder from "../lib/request";
 
@@ -35,7 +35,6 @@ export default class OneRepositoryScreen extends Component {
       this.setState({
         repo: repos.data.repo,
         contributors: contributors.data.contributors,
-        // readme: readme.data,
         isLoading: false
       });
     } catch(err) {
@@ -55,17 +54,57 @@ export default class OneRepositoryScreen extends Component {
   };
 
   render() {
-    const oneRepo = this.state.repo;
-    const oneRepoContributors = this.state.contributors;
+    let {repo} = this.state;
+    repo.contributors = this.state.contributors;
+    repo.language = "javascript";
 
     if(this.state.isLoading){
       return <Spinner/>
     }
 
 
-
     return (
       <Container>
+
+        <View style={styles.header}>
+
+          <View style={styles.title}>
+            <H1>{repo.name}</H1>
+            <H3>{(repo.language) && repo.language}</H3>
+          </View>
+
+          <View style={styles.title}>
+            <Thumbnail large source={{uri: repo.owner.avatar_url}} />
+            <H3>{repo.owner.login}</H3>
+          </View>
+
+
+          <View style={styles.stats}>
+
+            <View style={styles.stat}>
+              <Text>{repo.watchers_count}</Text>
+              <Octicons name="eye" size={20}/>
+            </View>
+
+            <View style={styles.stat}>
+              <Text>{repo.forks_count}</Text>
+              <Octicons name="repo-forked" size={20}/>
+            </View>
+
+            <View style={styles.stat}>
+              <Text>{repo.stargazers_count}</Text>
+              <Octicons name="star" size={20}/>
+            </View>
+
+          </View>
+
+
+
+        </View>
+
+        <View style={styles.body}>
+
+        </View>
 
       </Container>
     );
@@ -73,5 +112,42 @@ export default class OneRepositoryScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+
+  header: {
+    height: "50%",
+    width: "100%",
+    backgroundColor: "#9cdaee4d",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems:'center',
+  },
+
+  title: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+
+  stats: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: "100%",
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+
+  stat: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  body: {
+
+  }
+
 
 });
