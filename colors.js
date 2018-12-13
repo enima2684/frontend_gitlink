5aa7064a8519a04e385e48912b6429a2cde2b975
amine <bouamama.amine@bcg.com>
2018-12-13 18:28:04 +0100

    checkpoint

--- colors.js
+++ colors.js
@@ -2,7 +2,9 @@ export default colors = {
   GrayDark: "#23292D",
   BlueLight: "#79a1bd",
   Blue: '#313B72',
+  BlueTransparent: 'rgba(49, 59, 114, 0.9)',
   GreenFlashy: '#28A745',
+  GreenFlashyBis: '#34d058',
   GreenMat: '#62A87C',
   GreenLight: '#7EE981',
   whiteFont: '#fff'
--- views/ProfileScreen.js
+++ views/ProfileScreen.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import { Image, TouchableOpacity, StyleSheet, Text, View, Alert, ImageBackground } from "react-native";
+import { Image, TouchableOpacity, StyleSheet, Text, View, Alert, ImageBackground, ScrollView } from "react-native";
 import Octicons from "@expo/vector-icons/Octicons";
 import {LinearGradient} from 'expo';
-import { Container, H1, Spinner, Content, Button } from "native-base";
+import { Container, H1, Spinner, Content, Button, List, ListItem, Thumbnail, Body, Left, Right } from "native-base";
 import {StatusBar} from "react-native";
-
 
 
 import {authService} from "../lib/Authentication";
 import requestBuilder from "../lib/request";
+import colors from "../colors";
 
 
 export default class ProfileScreen extends Component {
@@ -98,30 +98,29 @@ export default class ProfileScreen extends Component {
     return (
       <Container>
         <StatusBar barStyle={"light-content"}/>
-        <Content>
 
-          <ImageBackground source={{uri: avatar_url}} style={styles.imageZone}>
+        <Content style={{height:"100%"}}>
 
-            {/* ------ USERNAME ------- */}
-            <View style={styles.usernameRow}>
-              <LinearGradient
-                colors={['#28A745', '#7EE981']}
-                start={[0.0, 1.0]}
-                end={[1.0, 0.5]}
-                locations={[0.0,  1.0]}
-                style={styles.usernameContainer}>
-                <Text style={styles.usernameText}>{login}</Text>
-              </LinearGradient>
-              <View style={styles.complementUsernameText}><Text>{" "}</Text></View>
-            </View>
+          {/* ------ BACKGROUND IMAGE + USERNAME ------- */}
+          {/*<View >*/}
+            <ImageBackground source={{uri: avatar_url}} style={styles.imageZone}>
 
-          </ImageBackground>
+              {/* ------ USERNAME ------- */}
+              <View style={styles.usernameRow}>
+                <View style={styles.usernameContainer}>
+                  <Text style={styles.usernameText}>{login}</Text>
+                </View>
+                <View style={styles.complementUsernameText}><Text>{" "}</Text></View>
+              </View>
+
+            </ImageBackground>
+          {/*</View>*/}
 
           {/* ------ BUTTONS ------- */}
           <View style={styles.buttonsRow}>
 
             <LinearGradient
-              colors={['#28A745', '#7EE981']}
+              colors={[colors.GreenFlashyBis, colors.GreenFlashy]}
               start={[0.0, 1.0]}
               end={[1.0, 0.5]}
               locations={[0.0,  1.0]}
@@ -133,19 +132,67 @@ export default class ProfileScreen extends Component {
 
             </LinearGradient>
 
-            <LinearGradient
-              colors={['#28A745', '#7EE981']}
-              start={[0.0, 1.0]}
-              end={[1.0, 0.5]}
-              locations={[0.0,  1.0]}
-              style={styles.btnContainer}>
+            {/*<LinearGradient*/}
+              {/*colors={[colors.GreenFlashyBis, colors.GreenFlashy]}*/}
+              {/*start={[0.0, 1.0]}*/}
+              {/*end={[1.0, 0.5]}*/}
+              {/*locations={[0.0,  1.0]}*/}
+              {/*style={styles.btnContainer}>*/}
 
-              <Button transparent style={styles.buttonElement} onPress={this.handleOnPressRepos}>
-                <Text style={styles.actionButton_text}>Repositories</Text>
-              </Button>
+              {/*<Button transparent style={styles.buttonElement} onPress={this.handleOnPressRepos}>*/}
+                {/*<Text style={styles.actionButton_text}>Repositories</Text>*/}
+              {/*</Button>*/}
 
-            </LinearGradient>
+            {/*</LinearGradient>*/}
 
+
+          </View>
+
+          {/*/!* ———————   BOTTOM CARD ————————*!/*/}
+          <View style={styles.body}>
+
+            <View style={styles.list}>
+
+              <ListItem button noBorder={true}  style={styles.listItem} onPress={() => alert('hey')}>
+
+                <View style={styles.listItem__left}>
+                  <Octicons name={"repo"} size={40} color={colors.Blue}/>
+                  <Text style={styles.list__key}>Repositories</Text>
+                </View>
+
+                <View>
+                  <Text style={styles.list__value}>{public_repos}</Text>
+                </View>
+
+              </ListItem>
+
+              <ListItem button noBorder={true}  style={styles.listItem}>
+
+                <View style={styles.listItem__left}>
+                  <Octicons name={"organization"} size={40} color={colors.Blue}/>
+                  <Text style={styles.list__key}>Followers</Text>
+                </View>
+
+                <View>
+                  <Text style={styles.list__value}>{followers}</Text>
+                </View>
+
+              </ListItem>
+
+              <ListItem button noBorder={true}  style={styles.listItem}>
+
+                <View style={styles.listItem__left}>
+                  <Octicons name={"screen-normal"} size={40} color={colors.Blue}/>
+                  <Text style={styles.list__key}>Following</Text>
+                </View>
+
+                <View>
+                  <Text style={styles.list__value}>{following}</Text>
+                </View>
+
+              </ListItem>
+
+            </View>
 
           </View>
 
@@ -159,10 +206,18 @@ const styles = StyleSheet.create({
 
   imageZone:{
     width: '100%',
-    height: 300,
+    height: 250,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
+
+    shadowColor: colors.GrayDark,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.7,
+    shadowRadius: 2,
+    // zIndex:1,
+    // elevation: 1,
+
   },
 
   usernameRow:{
@@ -179,12 +234,7 @@ const styles = StyleSheet.create({
     paddingLeft: 30,
     paddingRight: 30,
     borderRadius: 15,
-
-    shadowColor: '#23292D',
-    shadowOffset: { width: 1, height: 1 },
-    shadowOpacity: 0.7,
-    shadowRadius: 2,
-    elevation: 1,
+    backgroundColor: colors.BlueTransparent,
 
     flexShrink: 1,
   },
@@ -194,6 +244,7 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 20,
     backgroundColor: 'transparent',
+    letterSpacing: 1.7
   },
   complementUsernameText:{
     backgroundColor: 'transparent',
@@ -207,20 +258,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     position: 'absolute',
     width:'100%',
-    top: 280,
-
+    top: 217.5,
+    zIndex:4,
+    elevation:4,
   },
   btnContainer:{
     borderRadius: 10,
     paddingLeft: 5,
     paddingRight: 5,
-    width: "30%",
+    width: "66%",
 
-    shadowColor: '#23292D',
+    shadowColor: colors.GrayDark,
     shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.7,
     shadowRadius: 2,
-    elevation: 1,
 
   },
   buttonElement:{
@@ -232,6 +283,61 @@ const styles = StyleSheet.create({
   actionButton_text:{
     color: 'white',
     fontWeight: '700',
-    textAlign: 'center'
+    textAlign: 'center',
+    letterSpacing: 1.2
+  },
+  body:{
+    height: "110%",
+    width: "100%",
+    position: 'absolute',
+    top: 235,
+    zIndex: 3,
+    elevation:3,
+    shadowColor: colors.whiteFont,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+
+    // backgroundColor: colors.whiteFont,
+    backgroundColor: colors.BlueLight,
+    borderRadius: 15,
+  },
+
+  list:{
+    marginTop: 30,
+    paddingLeft: "20%",
+    paddingRight: "20%",
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+
+  },
+  listItem:{
+    width: '100%',
+    marginLeft: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  listItem__left:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+
+  list__key:{
+    paddingLeft: 15,
+    fontSize: 16,
+    fontWeight: "700",
+    color: colors.GrayDark,
+    letterSpacing: 1.2
+  },
+  list__value: {
+    fontWeight: "700",
+    fontSize: 32,
+    color: colors.Blue,
+    paddingLeft: 15,
   }
 });
