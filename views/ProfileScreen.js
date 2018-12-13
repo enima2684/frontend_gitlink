5441b74e5932b28f0dd831b4c3826c617ebd9b88
amine <bouamama.amine@bcg.com>
2018-12-13 12:01:27 +0100

    position the buttons

--- views/ProfileScreen.js
+++ views/ProfileScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Image, TouchableOpacity, StyleSheet, Text, View, Alert, ImageBackground } from "react-native";
 import Octicons from "@expo/vector-icons/Octicons";
 import {LinearGradient} from 'expo';
-import { Container, H1, Spinner, Content } from "native-base";
+import { Container, H1, Spinner, Content, Button } from "native-base";
 
 
 
@@ -98,7 +98,6 @@ export default class ProfileScreen extends Component {
           <ImageBackground source={{uri: avatar_url}} style={styles.imageZone}>
 
             {/* ------ USERNAME ------- */}
-
             <View style={styles.usernameRow}>
               <LinearGradient
                 colors={['#28A745', '#7EE981']}
@@ -111,14 +110,41 @@ export default class ProfileScreen extends Component {
               <View style={styles.complementUsernameText}><Text>{" "}</Text></View>
             </View>
 
-
-
-            {/* ------ BUTTONS ------- */}
-            <View>
-              <Text>BUTTONS</Text>
-            </View>
-
           </ImageBackground>
+
+
+          {/* ------ BUTTONS ------- */}
+          <View style={styles.buttonsRow}>
+
+            <LinearGradient
+              colors={['#28A745', '#7EE981']}
+              start={[0.0, 1.0]}
+              end={[1.0, 0.5]}
+              locations={[0.0,  1.0]}
+              style={styles.btnContainer}>
+
+              <Button transparent style={styles.buttonElement}>
+                <Text style={styles.actionButton_text}>Follow</Text>
+              </Button>
+
+            </LinearGradient>
+
+
+            <LinearGradient
+              colors={['#28A745', '#7EE981']}
+              start={[0.0, 1.0]}
+              end={[1.0, 0.5]}
+              locations={[0.0,  1.0]}
+              style={styles.btnContainer}>
+
+              <Button transparent style={styles.buttonElement}>
+                <Text style={styles.actionButton_text}>Repositories</Text>
+              </Button>
+
+            </LinearGradient>
+
+
+          </View>
 
         </Content>
       </Container>
@@ -133,7 +159,7 @@ const styles = StyleSheet.create({
     height: 300,
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
 
   usernameRow:{
@@ -149,8 +175,8 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     paddingLeft: 30,
     paddingRight: 30,
-
     borderRadius: 15,
+
     shadowColor: '#23292D',
     shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.7,
@@ -168,11 +194,43 @@ const styles = StyleSheet.create({
   },
   complementUsernameText:{
     backgroundColor: 'transparent',
+  },
+
+
+  buttonsRow:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    position: 'absolute',
+    width:'100%',
+    top: 280,
+
+  },
+  btnContainer:{
+    borderRadius: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
+    width: "30%",
+
+    shadowColor: '#23292D',
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.7,
+    shadowRadius: 2,
+    elevation: 1,
+
+  },
+  buttonElement:{
+    width: '100%',
+    display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  actionButton_text:{
+    color: 'white',
+    fontWeight: '700',
+    textAlign: 'center'
   }
-
-
-
-
 
 
 
